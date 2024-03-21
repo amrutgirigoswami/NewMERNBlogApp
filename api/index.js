40589bbe6e4442c82e5ad02aacfcf097fbef7517
Amrutgiri Goswami <amrutgiri.goswami@excellentwebworld.in>
2024-03-21 18:41:10 +0530

    connection on mongo db setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Mongo DB Connected Successfully');
+}).catch(err => {
+    console.log(err);
+});
+
 
 const app = express();
 
