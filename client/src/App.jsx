c0b5e8aa5674208091a7717a39f0f6ff18c48b27
Amrutgiri Goswami <amrutgiri.goswami@excellentwebworld.in>
2024-03-21 17:29:54 +0530

    Complete Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -6,10 +6,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/about-us' element={ <About /> } />
