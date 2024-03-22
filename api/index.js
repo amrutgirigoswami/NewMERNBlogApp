fc319d94b355e945ad9014150e868b1a5c4bd8de
Amrutgiri Goswami <amrutgiri.goswami@excellentwebworld.in>
2024-03-22 11:33:25 +0530

    create signup api and routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import UserRoutes from './Routes/UserRoutes.js'
-
+import AuthRoutes from './Routes/AuthRoutes.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -13,9 +13,10 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on 3000 port !');
 })
 
 app.use('/api/user', UserRoutes);
+app.use('/api/auth', AuthRoutes);
